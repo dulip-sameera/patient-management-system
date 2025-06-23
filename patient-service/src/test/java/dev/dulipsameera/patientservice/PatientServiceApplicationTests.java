494e0b1078629a3a025062bfac1a7d2c2203af03
Dulip Sameeera <dsgitrepo101@gmail.com>
2025-06-23 10:25:00 +0530

    init patient service

---
+++ patient-service/src/test/java/dev/dulipsameera/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dulipsameera.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
