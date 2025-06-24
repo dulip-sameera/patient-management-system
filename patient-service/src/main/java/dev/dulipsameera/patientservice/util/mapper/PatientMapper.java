5febfee602617bb740e97a44c2cc1ebe7e837fd1
Dulip Sameeera <dsgitrepo101@gmail.com>
2025-06-24 10:23:21 +0530

    create PatientRequestDTO to Patient mapper

--- patient-service/src/main/java/dev/dulipsameera/patientservice/util/mapper/PatientMapper.java
+++ patient-service/src/main/java/dev/dulipsameera/patientservice/util/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package dev.dulipsameera.patientservice.util.mapper;
 
+import dev.dulipsameera.patientservice.dto.PatientRequestDTO;
 import dev.dulipsameera.patientservice.dto.PatientResponseDTO;
 import dev.dulipsameera.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
 
@@ -13,5 +16,15 @@ public class PatientMapper {
         patientResponseDTO.setAddress(patient.getAddress());
         patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
         return patientResponseDTO;
+    }
+
+    public static Patient toPatient(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 }
