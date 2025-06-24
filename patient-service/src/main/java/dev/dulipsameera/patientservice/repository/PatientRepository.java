578e92cf895eac468e3834bd36828f66b6feaa16
Dulip Sameeera <dsgitrepo101@gmail.com>
2025-06-24 09:19:07 +0530

    add patient repository

---
+++ patient-service/src/main/java/dev/dulipsameera/patientservice/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package dev.dulipsameera.patientservice.repository;
+
+import dev.dulipsameera.patientservice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+}
