3ff34f716b235c018ba5db158d6d6ea630dc0c32
Dulip Sameeera <dsgitrepo101@gmail.com>
2025-06-25 17:07:52 +0530

    add user model

---
+++ auth-service/src/main/java/dev/dulipsameera/authservice/model/User.java
@@ -0,0 +1,29 @@
+package dev.dulipsameera.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
